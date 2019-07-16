ffc65f536e6db8e28980c7c7f6661b568f983c69
pp <17603003901@163.com>
2019-07-16 17:47:55 +0800

    first commit

---
+++ pages/commonProblem/commonProblem.vue
@@ -0,0 +1,23 @@
+<!--  常见问题 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/mine/myEvaluation/myEvaluation.vue
@@ -0,0 +1,23 @@
+<!--  我的评价 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
